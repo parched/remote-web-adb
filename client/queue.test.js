98b432ad8b7087d61e4a064844a7ec5eacf3dbf8
James Duley <jagduley@gmail.com>
2021-02-16 10:34:26 +0000

    Tidy client tests

--- client/queue.test.js
+++ client/queue.test.js
@@ -1,119 +1,159 @@
 const queue = require('./queue.js');
 
+console.assert = (statement, message) => {
+    if (!statement) throw new Error(message);
+};
+
+const range = function (begin, end) {
+    const result = new ArrayBuffer(end - begin);
+    for (let i = begin; i < end; ++i) {
+        new Uint8Array(result)[i - begin] = i;
+    }
+    return result;
+}
+
+expect.extend({
+    toBeUint8Range(received, begin, end) {
+        if (received.byteLength != end - begin) {
+            return {
+                message: () =>
+                    `expected length ${received.byteLength} to be ${end - begin}`,
+                pass: false,
+            };
+        }
+        for (let i = 0; i < end - begin; ++i) {
+            // TODO:
+            if (received.getUint8(i) != begin + i) {
+                return {
+                    message: () =>
+                        `expected element ${i} to be ${begin + i} but was ${received.getUint8(i)}`,
+                    pass: false,
+                };
+            }
+
+        }
+        return {
+            message: () =>
+                `expected range [${begin}, ${end})`,
+            pass: true,
+        };
+    },
+});
+
+
 test('queue write then read', async () => {
     const q = new queue.Queue();
-    q.write(new ArrayBuffer(5));
-    const result = await q.read(5);
-    expect(result.byteLength).toBe(5);
+
+    q.write(range(0, 5));
+
+    expect(await q.read(5)).toBeUint8Range(0, 5);
 });
 
 test('queue write then read 2 parts', async () => {
     const q = new queue.Queue();
-    q.write(new ArrayBuffer(5));
 
-    const result = await q.read(2);
-    expect(result.byteLength).toBe(2);
-    const result1 = await q.read(3);
-    expect(result1.byteLength).toBe(3);
+    q.write(range(0, 5));
+
+    expect(await q.read(2)).toBeUint8Range(0, 2);
+    expect(await q.read(3)).toBeUint8Range(2, 5);
 });
 
 test('queue write 2 parts then read', async () => {
     const q = new queue.Queue();
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(3));
 
-    const result = await q.read(5);
-    expect(result.byteLength).toBe(5);
+    q.write(range(0, 2));
+    q.write(range(2, 5));
+
+    expect(await q.read(5)).toBeUint8Range(0, 5);
 });
 
 test('queue write 3 parts then read', async () => {
     const q = new queue.Queue();
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(3));
-    q.write(new ArrayBuffer(4));
 
-    const result = await q.read(9);
-    expect(result.byteLength).toBe(9);
+    q.write(range(0, 2));
+    q.write(range(2, 5));
+    q.write(range(5, 9));
+
+    expect(await q.read(9)).toBeUint8Range(0, 9);
 });
 
 test('queue write 2 parts extra then read', async () => {
     const q = new queue.Queue();
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(7));
 
-    const result = await q.read(5);
-    expect(result.byteLength).toBe(5);
-    const result1 = await q.read(4);
-    expect(result1.byteLength).toBe(4);
+    q.write(range(0, 2));
+    q.write(range(2, 9));
+
+    expect(await q.read(5)).toBeUint8Range(0, 5);
+    expect(await q.read(4)).toBeUint8Range(5, 9);
 });
 
-test('queue read then write', async (done) => {
+test('queue read then write', () => {
     const q = new queue.Queue();
-    q.read(5).then(result => {
-        expect(result.byteLength).toBe(5);
-        done();
-    });
+    const result = expect(q.read(5)).resolves.toBeUint8Range(0, 5);
 
-    q.write(new ArrayBuffer(5));
+    q.write(range(0, 5));
+
+    return result;
 });
 
-test('queue write then read then write', async (done) => {
+test('queue write then read then write', () => {
     const q = new queue.Queue();
 
-    q.write(new ArrayBuffer(2));
+    q.write(range(0, 2));
 
-    q.read(5).then(result => {
-        expect(result.byteLength).toBe(5);
-        done();
-    });
+    const result = expect(q.read(5)).resolves.toBeUint8Range(0, 5);
 
-    q.write(new ArrayBuffer(3));
+    q.write(range(2, 5));
+
+    return result;
 });
 
-test('queue read then write 2 parts', async (done) => {
+test('queue read then write 2 parts', () => {
     const q = new queue.Queue();
-    q.read(5).then(result => {
-        expect(result.byteLength).toBe(5);
-        done();
-    });
 
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(3));
+    const result = expect(q.read(5)).resolves.toBeUint8Range(0, 5);
+
+    q.write(range(0, 2));
+    q.write(range(2, 5));
+
+    return result;
 });
 
-test('queue read then write 2 parts with extra', async (done) => {
+test('queue read then write 2 parts with extra', () => {
     const q = new queue.Queue();
-    q.read(5).then(result => {
-        expect(result.byteLength).toBe(5);
-        done();
-    });
 
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(7));
+    const result = expect(q.read(5)).resolves.toBeUint8Range(0, 5);
+
+    q.write(range(0, 2));
+    q.write(range(2, 9));
+
+    return result;
 });
 
-test('queue read then write 3 parts', async (done) => {
+test('queue read then write 3 parts', () => {
     const q = new queue.Queue();
-    q.read(10).then(result => {
-        expect(result.byteLength).toBe(10);
-        done();
-    });
 
-    q.write(new ArrayBuffer(2));
-    q.write(new ArrayBuffer(3));
-    q.write(new ArrayBuffer(5));
+    const result = expect(q.read(10)).resolves.toBeUint8Range(0, 10);
+
+    q.write(range(0, 2));
+    q.write(range(2, 5));
+    q.write(range(5, 10));
+
+    return result;
 });
 
 
-test('queue read 2 parts then write', async (done) => {
+test('queue read 2 parts then write', () => {
     const q = new queue.Queue();
-    q.read(2).then(result => {
-        expect(result.byteLength).toBe(2);
+
+    const result = q.read(2).then(result => {
+        expect(result).toBeUint8Range(0, 2);
         return q.read(3);
     }).then(result => {
-        expect(result.byteLength).toBe(3);
-        done();
+        expect(result).toBeUint8Range(2, 5);
     });
 
-    q.write(new ArrayBuffer(5));
+    q.write(range(0, 5));
+
+    return result;
 });
